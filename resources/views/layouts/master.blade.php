5fde92bda0b34596fa885f82fa9d33034ec35a3b
kiol1812 <kioly.vikerg0412@gmail.com>
2024-09-15 18:23:53 +0800

    ✨ Feat: layout, use blade

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Heroic Features - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href= {{ asset('home/assets/favicon.ico') }} />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href= {{ asset('home/css/styles.css')  }} rel="stylesheet" />
+    </head>
+    <body>
+    @include('layouts.components.responsive_navber')
+
+    @yield('page-content')
+
+    @include('layouts.components.footer')
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src= {{ asset('home/js/scripts.js') }} ></script>
+    </body>
+</html>
