acf5289f5df83cf5f03138a5f748da2fee45bffe
kiol1812 <kioly.vikerg0412@gmail.com>
2024-09-15 16:00:41 +0800

    ✨ Feat: route, without controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{path?}', function ($path='welcome') { // add ? mean can use url without $path
+    $allowedPath = ['welcome'];
+    if(in_array($path, $allowedPath)) return view($path);
+    else return "page didn't exit";
 });
+
+/*
+ * "str" v.s. 'str'
+ * $var=5
+ * "$var" -> 5
+ * '$var' -> $var
+ */
+
+Route::prefix('father')->group(function () {
+    Route::get('/{path?}', function ($path='son1') { // /father/{path?}
+        $allowedPath = ['son1', 'son2'];
+        if(in_array($path, $allowedPath)) return view('father_'.$path);
+        else return "page didn't exit";
+    });
+});
+
+Route::get('/name/{name?}', function ($name="test") {
+    return 'hello, '.$name; // . in this case is like +
+});
+
