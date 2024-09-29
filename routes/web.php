504d7494c3e87b4e34a371220fc055525ca4bb36
kiol1812 <kioly.vikerg0412@gmail.com>
2024-09-29 11:49:58 +0800

    ✨ Template v0.1.1

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,21 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', [HomeController::class, 'index']);
+use App\Http\Controllers\AdminHomeController;
+use App\Http\Controllers\AdminPostsController;
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
 Route::prefix('posts')->group(function () {
-    Route::get('/', [PostController::class, 'posts']);
+    Route::get('/', [PostController::class, 'posts'])->name('post.index');
     Route::get('/{id}', [PostController::class, 'post']);
 });
 
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [AdminHomeController::class, 'index'])->name("home.index");
+    Route::get('posts', [AdminPostsController::class, 'index'])->name("posts.index");
+    Route::get('posts/create', [AdminPostsController::class, 'create'])->name("posts.create");
+    Route::post('posts', [AdminPostsController::class, 'store'])->name("posts.store");
+    Route::get('posts/{post}/edit', [AdminPostsController::class, 'edit'])->name("posts.edit");
+    Route::patch('posts/{post}', [AdminPostsController::class, 'update'])->name("posts.update");
+    Route::delete('posts/{post}', [AdminPostsController::class, 'destroy'])->name("posts.destroy");
+});
